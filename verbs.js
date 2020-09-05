15b0a2f575ca8ce5f8a25b15f11f5359120c4f4a
pwn0x80 <pwnblank@gmail.com>
2020-09-05 19:44:32 +0530

    Initial commit

---
+++ verbs.js
@@ -0,0 +1,94 @@
+var vocab = {
+    0: {
+        verb: "müssen",
+        ich: "muss",
+        du: "musst",
+        er_sie_es: "muss",
+        wir: "müssen",
+        ihr: "müsst",
+        Sie: "müssen"
+    },
+    1: {
+        verb: "haben",
+        ich: "habe",
+        du: "hast",
+        er_sie_es: "hat",
+        wir: "haben",
+        ihr: "habt",
+        Sie: "haben"
+
+    },
+    2: {
+        verb: "können",
+        ich: "kann",
+        du: "kannst",
+        er_sie_es: "kann",
+        wir: "können",
+        ihr: "könnt",
+        Sie: "können"
+    },
+    3: {
+        verb: "sollen",
+        ich: "soll",
+        du: "sollst",
+        er_sie_es: "soll",
+        wir: "sollen",
+        ihr: "sollt",
+        Sie: "sollen"
+    },
+    4: {
+        verb: "wollen",
+        ich: "will",
+        du: "willst",
+        er_sie_es: "will",
+        wir: "wollen",
+        ihr: "wollt",
+        Sie: "wollen"
+    },
+    5: {
+        verb: "dürfen",
+        ich: "darf",
+        du: "darfst",
+        er_sie_es: "darf",
+        wir: "dürfen",
+        ihr: "dürft",
+        Sie: "dürfen"
+    },
+    6: {
+        verb: "werden",
+        ich: "werde",
+        du: "wirst",
+        er_sie_es: "wird",
+        wir: "werden",
+        ihr: "werdet",
+        Sie: "werden"
+    },
+    7: {
+        verb: "mögen",
+        ich: "mag",
+        du: "magst",
+        er_sie_es: "mag",
+        wir: "mögen",
+        ihr: "mögt",
+        Sie: "mögen"
+    },
+    8: {
+        verb: "nehmen",
+        ich: "nehme",
+        du: "nimmst",
+        er_sie_es: "nimmt",
+        wir: "nehmen",
+        ihr: "nehmt",
+        Sie: "nehmen"
+    },
+    9: {
+        verb: "essen",
+        ich: "esse",
+        du: "isst",
+        er_sie_es: "isst",
+        wir: "essen",
+        ihr: "esst",
+        Sie: "essen"
+    }
+   
+}
